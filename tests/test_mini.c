f6c1a66d16ee9eae6d4db55a851733b0d28a2651
jamesalbert <jalbert1@uci.edu>
2017-05-29 17:20:53 -0700

    test rough draft

--- tests/test_mini.c
+++ tests/test_mini.c
@@ -3,16 +3,27 @@
 #include "structures.h"
 #include "create.h"
 
-Test(mini, test_mini_probability) {
-  struct MiniMan * mm;
+struct MiniMan * mm;
+struct Alignment * A;
+
+void setup(void) {
   mm = malloc(sizeof(struct MiniMan));
+  A = malloc(sizeof(struct Alignment));
   mm->g1_file = "input/yeast.gw";
   mm->g2_file = "input/human.gw";
   mm->seq_file = "input/yeast_human.bitscores";
   mm->output_file = "output/mini.out.json";
   mm->alpha = 0.5;
   mm->time = 10;
-  struct Alignment * A;
-  A = malloc(sizeof(struct Alignment));
   create_alignment(A, mm);
+}
+
+void teardown(void) {
+  free(mm);
+  free(A);
+}
+
+Test(mini, test_mini_probability, .init = setup, .fini = teardown) {
+  double p = probability(A, 0.01, 99.9);
+  printf("%f\n", p);
 }
