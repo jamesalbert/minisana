2e5131169bcf7c6cf99112655c422599df9fe27e
jamesalbert <jalbert1@uci.edu>
2017-06-04 20:22:36 -0400

    dumb for loop

--- tests/test_mini.c
+++ tests/test_mini.c
@@ -22,8 +22,8 @@ void teardown(void) {
 }
 
 Test(mini, test_mini_probability, .init = setup, .fini = teardown) {
-  double p, t = 0.5;
-  for (double i = 0.0; i < mm->score; i += 0.0001) {
+  double i, p, t = 0.5;
+  for (i = 0.0; i < mm->score; i += 0.0001) {
     double p = probability(mm, i, t),
            e = exp(-(mm->score - i) / t);
     cr_assert(0 <= p && p <= 1, "probability outside range [0, 1]");
@@ -34,8 +34,9 @@ Test(mini, test_mini_probability, .init = setup, .fini = teardown) {
 
 Test(mini, test_mini_temperature, .init = setup, .fini = teardown) {
   double t = temperature(mm, 0.0);
+  int i;
   cr_assert(t = 1.0, "expected 1.0 temperature as base case");
-  for (int i = 1; i < 100; ++i) {
+  for (i = 1; i < 100; ++i) {
     t = temperature(mm, i);
     double e = exp(-T_DECAY * (i / (double)mm->time));
     cr_assert(t == e, "unexpected temperature");
